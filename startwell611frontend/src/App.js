8da34cd633643f636b7341c719cd51464d6307a0
akshata96 <akshata.akki96@gmail.com>
2021-03-10 18:33:16 -0500

    commit

--- startwell611frontend/src/App.js
+++ startwell611frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-import Homepage  from '../src/Pages/HomePage'
+import Homepage  from '../src/Pages/HomePage/HomePage'
 import logo from './logo.svg';
 import './App.css';
 
